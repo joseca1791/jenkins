2c531e1e09b3c2200329fa6ea6d269280f529c0f
JOSEDESKTOP\Jose Carlos <josecbarrantes@hotmail.com>
2018-07-04 19:06:25 -0600

    deleting publish output

---
+++ OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc; 
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
